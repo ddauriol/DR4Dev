5b1e675956fbbdc5b127eb3c1f225602d5ccbccc
Douglas Maciel d'Auriol Souza <ddauriol@gmail.com>
2020-05-10 09:25:38 -0300

    Chrome Extension

--- README_pt_BR.md
+++ README_pt_BR.md
@@ -1,8 +1,8 @@
 # ![DRDev](./docs/imgs/Icon-64.png) DR4Dev - Recursos de projeto para desenvolvedores
-  
+
 Os recursos de design para desenvolvedores são uma seleção de mais de 400 sites para facilitar os desenvolvedores.
 Uma lista com curadoria de recursos de design gratuito e ui para desenvolvedores, incluindo fotos de ações, modelos, frameworks, Kits de UI, ferramentas online e muito mais.
-  
+
 <p align="center">
   <img width="314" height="343" src="./docs/imgs/pt-br.png">
 </p>
@@ -44,17 +44,27 @@ Rápida, OffLine e direta.
 <p align="center">
   <img src="./docs/imgs/DR4Dev_main.gif">
 </p>
-  
+
+## Extensão para Firefox
+
+Rápida, OffLine e direta.
+
+<p align="center">
+  <img src="./docs/imgs/DR4Dev.gif">
+</p>
+<p align="center">
+  <img src="./docs/imgs/DR4Dev_main.gif">
+</p>
+
 ## Por Fazer:
-- [ ] Extensão para Chrome
+
+- [x] Extensão para Chrome
 - [ ] Api
 - [ ] Mais categorias
 - [ ] Inserir sistema de favoritos
-  
-### Inspiração  
+
+### Inspiração
+
 [OfficialMarinho/Links-uteis](https://github.com/OfficialMarinho/Links-uteis)
-  
+
 [bradtraversy/design-resources-for-developers](https://github.com/bradtraversy/design-resources-for-developers)
-
-
-
--- src/firefox/assets/js/popup.js
+++ src/firefox/assets/js/popup.js
@@ -1,150 +1,160 @@
-const container = document.getElementById("container")
-const search_btn = document.getElementById("search_btn")
-const search_input = document.getElementById("search_input");
-const tags = document.getElementById("tags");
-const logo_init = document.querySelector(".logo-init")
+const container = document.getElementById("container_main");
+const btSearch = document.getElementById("search_btn");
+const inputSearch = document.getElementById("search_input");
+const divKeyWords = document.getElementById("tags");
+const classLogoInit = document.querySelector(".logo-init");
+const searchForm = document.getElementById("search_form");
 
-let jsonData
+/*global browser*/
+const firefox = browser.i18n;
+
+let jsonData;
 
 function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', './json/db.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "./json/db.json", true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
 
 loadJSON(function (response) {
-    jsonData = JSON.parse(response);
+  jsonData = JSON.parse(response);
 });
 
-document.addEventListener('click', function (e) {
+document.addEventListener(
+  "click",
+  function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement,
-        TextId = target.id;
-    console.log(TextId)
-    if (TextId == 'search_btn' || TextId == 'search_btn_span') {
-        creatQuery()
+      TextId = target.id;
+    console.log(TextId);
+    if (TextId == "search_btn") {
+      creatQuery();
     }
-    if (TextId.slice(0,3) == 'tag') {
-        search_input.value = TextId.slice(4)
-        creatQuery(TextId.slice(4))
+    if (TextId.slice(0, 3) == "tag") {
+      inputSearch.value = TextId.slice(4);
+      creatQuery(TextId.slice(4));
     }
-    if (TextId.slice(0,4) == 'star') {
-        // star(1, '${value['name']}')
-        TextId = TextId.slice(5)
-        TextId = TextId.slice(0,-1)
-        TextId = TextId.replace("'","").replace("'","")
-        TextId = TextId.split(",")
-        name_site = TextId[1]
-        stars_int = parseInt(TextId[0])
-        saveLocalStorage(name_site, stars_int)
-    }
-}, false);
+  },
+  false
+);
+
+searchForm.addEventListener(
+  "submit",
+  function (e) {
+    creatQuery();
+    e.preventDefault();
+  },
+  false
+);
 
 function renderCards(data) {
-    txtHTML = '<br><br>'
-    Object.entries(data).forEach(([key, value]) => {
-        txtKeys = '<h5 class="mt-1">'
-        Object.entries(value['keys']).forEach(([key, value_keys]) => {
-            txtKeys += `<a id="tag-${value_keys}" href="#" class="badge badge-red ml-2">${value_keys}</a>`
-        })
-        txtKeys += '</h5>'
-        txtHTML += `
+  let txtHTML = "<br><br>";
+  Object.entries(data).forEach(([key, value]) => {
+    let txtKeys = "<h5 class='mt-1'>";
+    Object.entries(value["keys"]).forEach(([key, value_keys]) => {
+      txtKeys += `<a id="tag-${value_keys}" href="#" class="badge badge-red ml-2">${value_keys}</a>`;
+    });
+    txtKeys += "</h5>";
+    txtHTML += `
         <div class="card w-85 mb-2">
         <div class="row mb-0">
           <div class="col-3">
-            <img class="logo" src="./assets/favicon/${value['thumbnail']}">
+            <img class="logo" src="./assets/favicon/${value["thumbnail"]}">
           </div>
           <div class="col-9">
             <div class="card-body">
-              <h5 class="card-title">${value['name']}</h5>
-              <p class="card-text">${value['description']}</p>
+              <h5 class="card-title">${value["name"]}</h5>
+              <p class="card-text">${value["description"]}</p>
               ${txtKeys}
             </div>
           </div>
         </div>
         <div class="card-footer card-footer-white text-center">
-          <a href="${value['url']}" class="btn btn-green w-75 oval-flat text-uppercase">
+          <a href="${value["url"]}" target="_blank" class="btn btn-green w-75 oval-flat text-uppercase">
             Open
           </a>
         </div>
       </div>
-        `
-    });
-    container.innerHTML = txtHTML
+        `;
+  });
+  container.innerHTML = txtHTML;
+  document.documentElement.scrollTop = 0;
 }
 
 function searchInJsonData(query) {
-    data = []
-    Object.entries(jsonData).forEach(([key, value]) => {
-        if (value['name'].toLowerCase().includes(query) == true || value['description'].toLowerCase().includes(query) == true) {
-            data.push(value)
-        } else {
-            Object.entries(value['keys']).forEach(([key, value_keys]) => {
-                if (value_keys.toLowerCase() == query.toLowerCase()) {
-                    data.push(value)
-                }
-            });
+  let data = [];
+  Object.entries(jsonData).forEach(([key, value]) => {
+    if (
+      value["name"].toLowerCase().includes(query) == true ||
+      value["description"].toLowerCase().includes(query) == true
+    ) {
+      data.push(value);
+    } else {
+      Object.entries(value["keys"]).forEach(([key, value_keys]) => {
+        if (value_keys.toLowerCase() == query.toLowerCase()) {
+          data.push(value);
         }
-    });
-    renderCards(data)
+      });
+    }
+  });
+  renderCards(data);
 }
 
 function creatQuery(query = "") {
-    if (query == ""){
-        query = search_input.value
-    }
-    searchInJsonData(query)
+  if (query == "") {
+    query = inputSearch.value;
+  }
+  searchInJsonData(query);
 }
 
-function showTags(){
-    tag_list = [
-        "Images",
-        "Videos",
-        "Tools",
-        "Project",
-        "Freebies",
-        "IDEs",
-        "Front-end",
-        "Icons",
-        "IDE Cloud",
-        "Langues",
-        "Style guide",
-        "Charges",
-        "Network",
-        "Study",
-        "Hosting",
-        "Pentest",
-        "Channels",
-        "Business",
-        "Fonts",
-        "Vector",
-        "Frameworks",
-        "UI",
-        "React",
-        "Vue",
-        "Angular",
-        "Svelte"
-    ]
-    txtTag = ''
-    Object.entries(tag_list).forEach(([key, tag]) => {
-        txtTag += `<a id="tag-${tag}" href="#" class="badge badge-green ml-2 mt-2">${tag}</a>`
-    })
-    tags.innerHTML = txtTag
+function showTags() {
+  let keyWords = [
+    "Images",
+    "Videos",
+    "Tools",
+    "Project",
+    "Freebies",
+    "IDEs",
+    "Front-end",
+    "Icons",
+    "IDE Cloud",
+    "Langues",
+    "Style guide",
+    "Charges",
+    "Network",
+    "Study",
+    "Hosting",
+    "Pentest",
+    "Channels",
+    "Business",
+    "Fonts",
+    "Vector",
+    "Frameworks",
+    "UI",
+    "React",
+    "Vue",
+    "Angular",
+    "Svelte",
+  ];
+  let txtWord = "";
+  Object.entries(keyWords).forEach(([key, word]) => {
+    txtWord += `<a id="tag-${word}" href="#" class="badge badge-green ml-2 mt-2">${word}</a>`;
+  });
+  divKeyWords.innerHTML = txtWord;
 }
 
-function translaterPage(){
-    search_btn.innerHTML = browser.i18n.getMessage("btn_search")
-    search_input.placeholder = browser.i18n.getMessage("input_search")
-    lang = browser.i18n.getMessage("langue")
-    logo_init.style.backgroundImage = `url(./assets/logo/${lang}/logo.png)`;
-
+function translaterPage() {
+  btSearch.innerHTML = firefox.getMessage("btn_search");
+  inputSearch.placeholder = firefox.getMessage("input_search");
+  let locale = firefox.getMessage("langue");
+  classLogoInit.style.backgroundImage = `url(./assets/logo/${locale}/logo.png)`;
 }
 
-showTags()
-translaterPage()
+showTags();
+translaterPage();
